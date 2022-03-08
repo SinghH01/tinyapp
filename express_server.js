eb498ee2270a4718f2560a2e52362e43acfc2262
Harshbir Singh <singh.hveer1@gmail.com>
2022-03-08 04:12:26 -0700

    Implement functionality to GET and POST form page

--- express_server.js
+++ express_server.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+// Require body parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 // Set ejs as view engine
 app.set("view engine", "ejs");
 
@@ -24,11 +28,23 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", tempelateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send(generateRandomString());         // Respond with 'Ok' (we will replace this)
+});
+
+// GET route to render the urls_new.ejs
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 //To display single url and its shortened form
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+
 
 // Send JSON string representing the entire urlDatabase object
 app.get("/urls.json", (req, res) => {
@@ -44,3 +60,13 @@ app.listen(PORT, () => {
   console.log(`App listenig on port ${PORT}!`);
 });
 
+//Returns a string of 6 random alphanumeric characters
+function generateRandomString() {
+  let randomString = "";  
+  let letters ='abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  for(let i = 0; i < 6; i++) {    
+     randomString += letters[Math.floor(Math.random() * letters.length)];
+  }
+  return randomString;
+}
