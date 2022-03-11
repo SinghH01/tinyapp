5d9bc9dffeee4b89db798327562f3809480133ca
Harshbir Singh <singh.hveer1@gmail.com>
2022-03-11 00:53:22 -0700

    Handle registration errors

--- express_server.js
+++ express_server.js
@@ -3,38 +3,64 @@ const express = require("express");
 const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
-
-// Require body parser
-const bodyParser = require("body-parser");
-const { cookie } = require("request");
+const bodyParser = require("body-parser"); // Require body parser
 app.use(bodyParser.urlencoded({extended: true}));
+//const { cookie } = require("request");
 
 // Set ejs as view engine
 app.set("view engine", "ejs");
 
 app.use(cookieParser());
 
-// URL Databse Object
+// URL Databse
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-//Display all urls in browser
-app.get("/urls", (req, res) => {
-  const tempelateVars = {username: req.cookies.username, urls: urlDatabase};
-  res.render("urls_index", tempelateVars);
+//User Database
+let users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+// POST Requests
+
+// Save new user to database
+app.post("/register", (req, res) => {
+  if (req.body.email === "" || req.body.password === "") {  // Check if email or password is empty string
+    res.status(404).send("Email or Password cannot be empty");
+  } else {
+    let emailCheck = checkDuplicateEmail(req.body.email); // Check if email alreday exist's in users database
+    if (emailCheck === false) {
+      const randomID = generateRandomString();
+      users[randomID] = {id: randomID, email: req.body.email, password: req.body.password};
+      res.cookie('user_id', randomID);
+      res.redirect('/urls');
+    } else {
+      res.status(404).send("Email already exist!");
+    }
+
+  }
 });
 
 app.post("/login", (req, res) => {
-  res.cookie('username', req.body.username)  
+  res.cookie('username', req.body.username);
   res.redirect('/urls');
 });
 
-app.post("/logout", (req, res) => {  
-  res.clearCookie("username");
+app.post("/logout", (req, res) => {
+  res.clearCookie("user_id");
   res.redirect('/urls');
-})
+});
 
 //Add new longURLS and it's corresponding randomly generated short url in database using form
 app.post("/urls", (req, res) => {
@@ -54,15 +80,28 @@ app.post("/urls/:shortURL/edit", (req, res) => {
   res.redirect("/urls");
 });
 
+// GET Requests
+
+//Display all urls in browser
+app.get("/urls", (req, res) => {
+  const templateVars = {user: users[req.cookies.user_id], urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
+
+app.get("/register", (req, res) => {
+  const templateVars = {user: users[req.cookies.user_id]};
+  res.render("urls_register", templateVars);
+});
+
 // Render the page to add new url's
 app.get("/urls/new", (req, res) => {
-  const tempelateVars = {username: req.cookies.username}
-  res.render("urls_new", tempelateVars);
+  const templateVars = {user: users[req.cookies.user_id]};
+  res.render("urls_new", templateVars);
 });
 
 //To display single url and its shortened form
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { username: req.cookies.username, shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { user: users[req.cookies.user_id], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   //If a client requests a non-existent shortURL
   if (urlDatabase[req.params.shortURL]) {
     res.render("urls_show", templateVars);
@@ -74,7 +113,7 @@ app.get("/urls/:shortURL", (req, res) => {
 
 // Use shortUrl to redirect the page to its corresonding website (longUrl)
 app.get("/u/:shortURL", (req, res) => {
-  const templateVars = {username: req.cookies.username, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = {user: users[req.cookies.user_id], longURL: urlDatabase[req.params.shortURL] };
   //If a client requests a non-existent shortURL
   if (urlDatabase[req.params.shortURL]) {
     res.redirect(templateVars.longURL);
@@ -82,6 +121,8 @@ app.get("/u/:shortURL", (req, res) => {
     res.status(404).send('Page Not Found');
   }
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`App listenig on port ${PORT}!`);
@@ -96,4 +137,14 @@ function generateRandomString() {
     randomString += letters[Math.floor(Math.random() * letters.length)];
   }
   return randomString;
+}
+
+// Returns true if email already exist in users database
+function checkDuplicateEmail(email) {
+  for (const mail in users) {
+    if (users[mail].email === email) {
+      return true;
+    }
+  }
+  return false;
 }
